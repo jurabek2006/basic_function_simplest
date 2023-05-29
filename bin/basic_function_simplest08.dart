4093a8b82924dc1353ff5a359b3c447bb58b51f2
jurabek2006 <yuldoshevjurabek@gmail.com>
2023-05-29 15:00:03 +0500

    h

--- bin/basic_function_simplest08.dart
+++ bin/basic_function_simplest08.dart
@@ -1,3 +1,19 @@
 //Task 8:
 //Create a function named printSquareNumbers that prints the square of numbers from 1 to 10.
-void main() {}
+printSquareNumbers() {
+    print(1 * 1);
+    print(2 * 2);
+    print(3 * 3);
+    print(4 * 4);
+    print(5 * 5);
+    print(6 * 6);
+    print(7 * 7);
+    print(8 * 8);
+    print(9 * 9);
+    print(10 * 10);
+}
+
+
+void main() {
+    print(printSquareNumbers());
+}
