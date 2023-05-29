7f782b87e671e48fa7a0cf2a6bc59488f71771af
jurabek2006 <yuldoshevjurabek@gmail.com>
2023-05-29 15:01:59 +0500

    k

--- bin/basic_function_simplest09.dart
+++ bin/basic_function_simplest09.dart
@@ -1,3 +1,18 @@
 //Task 9:
 //Write a function named printReverseNumbers that prints the numbers from 10 to 1 in reverse order.
-void main() {}
+printReverseNumbers() {
+    print(10);
+    print(9);
+    print(8);
+    print(7);
+    print(6);
+    print(5);
+    print(4);
+    print(3);
+    print(2);
+    print(1);
+}
+
+void main() {
+    print(printReverseNumbers());
+}
