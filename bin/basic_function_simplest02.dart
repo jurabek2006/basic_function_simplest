1e0df3e2a99fb4e6cb8e2d1fa36892c46af80b72
jurabek2006 <yuldoshevjurabek@gmail.com>
2023-05-29 14:53:16 +0500

    s

--- bin/basic_function_simplest02.dart
+++ bin/basic_function_simplest02.dart
@@ -1,3 +1,18 @@
 //Task 2:
 //Create a function named countdown that prints the numbers from 10 to 1 in descending order.
-void main() {}
+countdown() {
+    print(10);
+    print(9);
+    print(8);
+    print(7);
+    print(6);
+    print(5);
+    print(4);
+    print(3);
+    print(2);
+    print(1);
+}
+
+void main() {
+    print(countdown());
+}
