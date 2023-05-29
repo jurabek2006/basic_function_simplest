85f717ff4e236beba20c7f6b25c1439ecfbdacd7
jurabek2006 <yuldoshevjurabek@gmail.com>
2023-05-29 14:56:27 +0500

    s

--- bin/basic_function_simplest04.dart
+++ bin/basic_function_simplest04.dart
@@ -1,3 +1,18 @@
 //Task 4:
 //Create a function named printMultiplicationTable that prints the multiplication table for numbers 1 to 10.
-void main() {}
+printMultiplicationTable() {
+    print(1 * 2);
+    print(1 * 3);
+    print(1 * 4);
+    print(1 * 5);
+    print(1 * 6);
+    print(1 * 7);
+    print(1 * 8);
+    print(1 * 9);
+    print(1 * 10);
+  
+}
+
+void main() {
+    print(printMultiplicationTable());
+}
