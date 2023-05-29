0bee3a3b4825582263ceb39fb7f98c7e0d1b389e
jurabek2006 <yuldoshevjurabek@gmail.com>
2023-05-29 14:59:06 +0500

    g

--- bin/basic_function_simplest07.dart
+++ bin/basic_function_simplest07.dart
@@ -1,3 +1,19 @@
 //Task 7:
 //Write a function named printAlphabets that prints all the lowercase alphabets from a to z.
-void main() {}
+printAlphabets() {
+    print('a');
+    print('b');
+    print('d');
+    print('c');
+    print('e');
+    print('k');
+    print('j');
+    print('s');
+    print('g');
+    print('z');
+}
+
+
+void main() {
+    print(printAlphabets());
+}
