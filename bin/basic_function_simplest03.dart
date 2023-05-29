6bbc549667afd46108e3522ef43c50659165358f
jurabek2006 <yuldoshevjurabek@gmail.com>
2023-05-29 14:55:12 +0500

    a

--- bin/basic_function_simplest03.dart
+++ bin/basic_function_simplest03.dart
@@ -1,3 +1,19 @@
 //Task 3:
 //Write a function named printEvenNumbers that prints all even numbers from 1 to 20.
-void main() {}
+printEvenNumbers() {
+    print(2);
+    print(4);
+    print(6);
+    print(8);
+    print(10);
+    print(12);
+    print(14);
+    print(16);
+    print(18);
+    print(20);
+
+}
+
+void main() {
+    print(printEvenNumbers());
+}
