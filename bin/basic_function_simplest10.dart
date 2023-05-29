28670330a9aec0ddb5eb423944215e2d2cfed825
jurabek2006 <yuldoshevjurabek@gmail.com>
2023-05-29 15:02:49 +0500

    z

--- bin/basic_function_simplest10.dart
+++ bin/basic_function_simplest10.dart
@@ -1,3 +1,17 @@
 //Task 10:
 //Create a function named printDivisibleByThree that prints all the numbers from 1 to 30 that are divisible by 3.
-void main() {}
+printDivisibleByThree() {
+    print(6 / 3);
+    print(9 / 3);
+    print(12 / 3);
+    print(15 / 3);
+    print(18 / 3);
+    print(21 / 3);
+    print(24 / 3);
+    print(27 / 3);
+    print(30 / 3);
+}
+
+void main() {
+    print(printDivisibleByThree());
+}
