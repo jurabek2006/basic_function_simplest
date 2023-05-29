dbba1d799245ff246885777ade457a485fdbcec1
jurabek2006 <yuldoshevjurabek@gmail.com>
2023-05-29 14:58:10 +0500

    f

--- bin/basic_function_simplest06.dart
+++ bin/basic_function_simplest06.dart
@@ -1,3 +1,18 @@
 //Task 6:
 //Create a function named printFibonacci that prints the first 10 numbers in the Fibonacci sequence.
-void main() {}
+printFibonacci() {
+    print(1);
+    print(2);
+    print(3);
+    print(4);
+    print(5);
+    print(6);
+    print(7);
+    print(8);
+    print(9);
+    print(10);
+}
+
+void main() {
+    print(printFibonacci());
+}
